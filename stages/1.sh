6fb1a92b1a6d05688c362f15c0f62a99ec30d189
aldrinbalitbit <aldrinbalitbit1127@gmail.com>
2023-06-10 23:39:35 +0800

    Using above from series

--- stages/1.sh
+++ stages/1.sh
@@ -9,9 +9,9 @@ _msg "Preparing icepearl directores"
 mkdir -p $ICEPEARL_DIR/{build,toolchain,sources,rootfs,iso,initrd}
 
 # 1. binutils
-_msg "Downloading binutils"
+_msg "Cloning binutils"
 mkdir $ICEPEARL_SOURCES/binutils
-_fetch https://ftp.gnu.org/pub/gnu/binutils/binutils-2.40.tar.xz $ICEPEARL_SOURCES/binutils >> $ICEPEARL_TOOLCHAIN/build-log
+_clone master git://sourceware.org/git/binutils-gdb.git $ICEPEARL_SOURCES/binutils >> $ICEPEARL_TOOLCHAIN/build-log
 cd $ICEPEARL_SOURCES/binutils
 sed '6009s/$add_dir//' -i ltmain.sh
 
@@ -30,34 +30,19 @@ _make >> $ICEPEARL_TOOLCHAIN/build-log
 _msg "Installing binutils"
 _make_install >> $ICEPEARL_TOOLCHAIN/build-log
 
-# 2. linux-headers
-_msg "Downloading linux-headers"
-mkdir $ICEPEARL_SOURCES/linux-headers
-_fetch https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-6.1.32.tar.xz $ICEPEARL_SOURCES/linux-headers >> $ICEPEARL_TOOLCHAIN/build-log
-cd $ICEPEARL_SOURCES/linux-headers
-
-_msg "Building linux-headers"
-make ARCH=x86 mrproper >> $ICEPEARL_TOOLCHAIN/build-log
-
-_msg "Installing linux-headers"
-make ARCH=x86 INSTALL_HDR_PATH="${ICEPEARL_TOOLCHAIN}/usr" headers_install >> $ICEPEARL_TOOLCHAIN/build-log
-
-# 3. gcc-static
-_msg "Downloading gcc"
-mkdir $ICEPEARL_SOURCES/gcc
-_fetch https://ftp.gnu.org/pub/gnu/gcc/gcc-13.1.0/gcc-13.1.0.tar.xz $ICEPEARL_SOURCES/gcc >> $ICEPEARL_TOOLCHAIN/build-log
+# 2. gcc
+_msg "Cloning gcc"
+_clone Thesis git://gcc.gnu.org/git/gcc.git $ICEPEARL_SOURCES/gcc >> $ICEPEARL_TOOLCHAIN/build-log
 cd $ICEPEARL_SOURCES/gcc
 sed -e '/m64=/s/lib64/lib/' -i.orig gcc/config/i386/t-linux64
 
 _msg "Downloading gcc prerequisites"
 ./contrib/download_prerequisites
 
-_msg "Configuring gcc-static"
+_msg "Configuring gcc"
 mkdir $ICEPEARL_BUILD/gcc-static && cd $ICEPEARL_BUILD/gcc-static
 $ICEPEARL_SOURCES/gcc/configure --prefix=$ICEPEARL_TOOLCHAIN       \
-	                        --libdir=/lib                      \
-	                        --libexecdir=/lib                  \
-                                --target=$ICEPEARL_TARGET          \
+	                        --target=$ICEPEARL_TARGET          \
 				--with-sysroot=$ICEPEARL_TOOLCHAIN \
 				--with-newlib                      \
 				--without-headers                  \
@@ -76,29 +61,35 @@ $ICEPEARL_SOURCES/gcc/configure --prefix=$ICEPEARL_TOOLCHAIN       \
 				--disable-threads                  \
                                 --disable-werror >> $ICEPEARL_TOOLCHAIN/build-log
 
-_msg "Building gcc-static"
+_msg "Building gcc"
 _make >> $ICEPEARL_TOOLCHAIN/build-log
 
-_msg "Installing gcc-static"
+_msg "Installing gcc"
 _make_install >> $ICEPEARL_TOOLCHAIN/build-log
 
+# 3. linux-headers
+_msg "Cloning linux-headers"
+_clone linux-rolling-lts git://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git $ICEPEARL_SOURCES/linux-headers >> $ICEPEARL_TOOLCHAIN/build-log
+cd $ICEPEARL_SOURCES/linux-headers
+
+_msg "Building linux-headers"
+make mrproper
+
+_msg "Installing linux-headers"
+make INSTALL_HDR_PATH=$ICEPEARL_TOOLCHAIN/usr headers_install
+
 # 4. glibc
-_msg "Downloading glibc"
-mkdir $ICEPEARL_SOURCES/glibc
-_fetch https://ftp.gnu.org/pub/gnu/glibc/glibc-2.37.tar.xz $ICEPEARL_SOURCES/glibc
+_msg "Cloning glibc"
+_clone master git://sourceware.org/git/glibc.git $ICEPEARL_SOURCES/glibc >> $ICEPEARL_TOOLCHAIN/build-log
 
 _msg "Configuring glibc"
 mkdir $ICEPEARL_BUILD/glibc && cd $ICEPEARL_BUILD/glibc
 cat > configparms <<EOF
 slibdir=/usr/lib
-rtlddir=/usr/lib
-sbindir=/usr/bin
-rootsbindir=/usr/bin
+rootsbindir=/usr/sbin
 EOF
 $ICEPEARL_SOURCES/glibc/configure --prefix=/usr                                  \
-	                          --libdir=/usr/lib                              \
-				  --libexecdir=/usr/lib                          \
-				  --host=$ICEPEARL_TARGET                        \
+	                          --host=$ICEPEARL_TARGET                        \
 				  --with-headers=$ICEPEARL_TOOLCHAIN/usr/include \
 				  --enable-kernel=4.14                           \
 				  --disable-werror >> $ICEPEARL_TOOLCHAIN/build-log
@@ -112,18 +103,15 @@ _make_install $ICEPEARL_TOOLCHAIN >> $ICEPEARL_TOOLCHAIN/build-log
 _msg "Fixing hard coded path"
 sed '/RTLDLIST=/s@/usr@@g' -i $ICEPEARL_TOOLCHAIN/usr/bin/ldd
 
-# 5. libstdc++-v3
-mkdir $ICEPEARL_BUILD/libstdc++-v3 && cd $ICEPEARL_BUILD/libstdc++-v3
+# 5. libstdcpp
+mkdir $ICEPEARL_BUILD/libstdcpp && cd $ICEPEARL_BUILD/libstdcpp
 _msg "Configuring libstdc++-v3"
 $ICEPEARL_SOURCES/gcc/libstdc++-v3/configure --prefix=/usr           \
-                                             --libdir=/usr/lib       \
-                                             --libexecdir=/usr/lib   \
-					     --host=$ICEPEARL_TARGET \
+                                             --host=$ICEPEARL_TARGET \
 					     --disable-libstdcxx-pch \
 					     --disable-multilib      \
 					     --disable-nls           \
-					     --disable-werror        \
-					     --with-gxx-include-dir=/$ICEPEARL_TARGET/include/c++/13.1.0 >> $ICEPEARL_TOOLCHAIN/build-log
+					     --disable-werror >> $ICEPEARL_TOOLCHAIN/build-log
 
 _msg "Building libstdc++-v3"
 _make >> $ICEPEARL_TOOLCHAIN/build-log
